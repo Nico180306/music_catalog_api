de6a2aca94e617cc4367815a953ca5bf7a6236da
JaimeJJ <jaime.jimenez@uisek.edu.ec>
2026-07-14 22:16:37 -0500

    Fase 1: modelo Artist, serializer y viewset

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=150)
+    nationality = models.CharField(max_length=100, blank=True)
+    formed_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
