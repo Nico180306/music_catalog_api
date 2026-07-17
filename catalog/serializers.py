8d5d792b924db88c76cfdf6fc704763f005d43c9
Nicolas <nicolas.santillan@uisek.edu.ec>
2026-07-16 19:07:12 -0500

    feat: implementación de la entidad Album con relación 1:N con el artista, serializadores anidados y endpoints de API

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Artist
+from .models import Artist, Album
 
+# 1. PRIMERO definimos el álbum 
+class AlbumSerializer(serializers.ModelSerializer):
+    # Inyectamos el nombre del artista en el JSON para facilitarle la vida a React
+    artist_name = serializers.ReadOnlyField(source='artist.name')
 
+    class Meta:
+        model = Album
+        fields = ['id', 'artist', 'artist_name', 'title', 'release_date', 'price', 'cover_url']
+
+# 2. DESPUÉS definimos el artista, anidando el AlbumSerializer
 class ArtistSerializer(serializers.ModelSerializer):
+    # Relación inversa: Trae el catálogo del artista en una sola llamada REST
+    albums = AlbumSerializer(many=True, read_only=True)
+
     class Meta:
         model = Artist
-        fields = ['id', 'name', 'nationality', 'formed_date', 'bio']
+        # Campos exactos de models.py + la relación 'albums'
+        fields = ['id', 'name', 'nationality', 'formed_date', 'bio', 'albums']
