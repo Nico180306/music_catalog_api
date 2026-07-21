32c409589107a5ea29c09b7e6c30288f8da544a8
Nicolas <nicolas.santillan@uisek.edu.ec>
2026-07-21 17:18:30 -0500

    fix: exponer campo genre y asegurar estandarizacion del campo bio en serializador

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,10 +5,10 @@ from .models import Artist, Album
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'genre')
-    search_fields = ('name', 'genre')
+    search_fields = ('name', 'genre', 'bio')
 
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'artist', 'release_date')
     list_filter = ('artist', 'release_date')
-    search_fields = ('title',)
+    search_fields = ('title', 'artist__name')
