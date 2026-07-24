427fc1924d93da77ad93c71016c9723a8c4379b3
Nicolas <nicolas.santillan@uisek.edu.ec>
2026-07-23 21:31:10 -0500

    Actualización de puertos para Docker

--- music_api/settings.py
+++ music_api/settings.py
@@ -61,6 +61,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://localhost:8000",
 ]
 
 ROOT_URLCONF = 'music_api.urls'
